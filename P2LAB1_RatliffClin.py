c183f904d42f1a00839261ffdea617764fe8193f
ratliffcolin <160803570+ratliffcolin@users.noreply.github.com>
2024-02-27 14:16:49 -0500

    Add files via upload

---
+++ P2LAB1_RatliffClin.py
@@ -0,0 +1,18 @@
+# Colin Ratliff
+# 2/27/2024
+# Variables and Expressions
+# P2LAB1
+import math
+radius=float(input("What is the radius of the circle?"))
+print()
+diameter=2*radius
+print('The diameter of the circle is {diameter:.1f}')
+print()
+circumference=2*3.14*radius
+print('The circumference of the circle is {circumference:,.2f}')
+print()
+area=3.14*math.pow(radius,2)
+print(f'The area of the circle is {area:.3f}')
+print('The area of the circle is',format(area,'.3f'))
+
+      
